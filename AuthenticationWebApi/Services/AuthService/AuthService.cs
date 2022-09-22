dfb7130aed14052f92663336262f842bf571e67a
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-09-22 18:32:36 +0600

    passwordHash and passwordSalt setup done

--- AuthenticationWebApi/Services/AuthService/AuthService.cs
+++ AuthenticationWebApi/Services/AuthService/AuthService.cs
@@ -1,11 +1,27 @@
-﻿namespace AuthenticationWebApi.Services.AuthService
+﻿using System.Security.Cryptography;
+
+namespace AuthenticationWebApi.Services.AuthService
 {
     public class AuthService : IAuthService
     {
         public async Task<User> RegisterUser(UserDto request)
         {
-            var user = new User { UserName = request.UserName };
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordsalt);
+            var user = new User { 
+                UserName = request.UserName ,
+                PasswordHash = passwordHash,
+                PassworSalt = passwordsalt
+            };
             return user;
+        }
+
+        private void CreatePasswordHash (string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
         }
     }
 }
